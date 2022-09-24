1e32abb2c7f0ec126e378f6444e87c446cfc2bf9
lenak <lenakla@ukr.net>
2022-09-24 19:07:11 +0300

    MapStruct

--- src/main/java/com/example/demowithtests/domain/Employee.java
+++ src/main/java/com/example/demowithtests/domain/Employee.java
@@ -16,6 +16,7 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
     private String country;
     private String email;
