95817a9e4a4fda7c58a096bb4ad907046e0b2038
lenak <lenakla@ukr.net>
2022-09-25 16:08:06 +0300

    в мапстракт сделан комент

--- src/main/java/com/example/demowithtests/util/config/mapstruct/EmployeeMapper.java
+++ src/main/java/com/example/demowithtests/util/config/mapstruct/EmployeeMapper.java
@@ -2,9 +2,9 @@ package com.example.demowithtests.util.config.mapstruct;
 
 import com.example.demowithtests.domain.Employee;
 import com.example.demowithtests.dto.*;
-import org.mapstruct.Mapper;
+//import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+//@Mapper(componentModel = "spring")
 public interface EmployeeMapper {
 
     EmployeeRead1Dto employeeRead1Dto(Employee employee);
